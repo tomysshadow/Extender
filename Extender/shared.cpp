9d418dde56a33d7d8f0ab7339394842344b46053
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2022-10-10 20:48:57 -0600

    correct some error messages

--- Extender/shared.cpp
+++ Extender/shared.cpp
@@ -3,26 +3,6 @@
 #include <sstream>
 #include <windows.h>
 
-bool shiftMemory(size_t bufferSize, void* buffer, size_t sourceSize, void* source, size_t shift, bool direction) {
-	if (source < buffer || (char*)source + sourceSize >= (char*)buffer + bufferSize) {
-		return false;
-	}
-
-	size_t destinationSize = (char*)buffer + bufferSize - source;
-	char* destination = (char*)source;
-
-	if (direction) {
-		destination += shift;
-	} else {
-		destination -= shift;
-	}
-
-	if (destination < buffer || destination + destinationSize >= (char*)buffer + bufferSize) {
-		return false;
-	}
-	return !memmove_s(destination, destinationSize, source, sourceSize);
-}
-
 static LPCSTR MESSAGE_BOX_CAPTION = "Extender Error";
 
 bool showLastError(LPCSTR errorMessage) {
